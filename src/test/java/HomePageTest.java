c5a52623617b80ed33f2cb7913ec73c34ef742c3
zahareliseev <zakh-e@yandex.ru>
2020-10-13 00:44:55 +0300

    Added assertion

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,16 +1,20 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hamcrest.Matchers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class HomePageTest {
 
     private static final Logger logger = LogManager.getLogger(HomePageTest.class);
     protected static WebDriver driver;
+    private final static String HOMEPAGE_TITLE = "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
 
     @BeforeTest
     public void setUp() {
@@ -23,6 +27,7 @@ public class HomePageTest {
     public void openHomePage() {
         driver.get("https://otus.ru");
         logger.info("Main page was open");
+        assertThat(driver.getTitle(), Matchers.equalTo(HOMEPAGE_TITLE));
     }
 
     @AfterTest
