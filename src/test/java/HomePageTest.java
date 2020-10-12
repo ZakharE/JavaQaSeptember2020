3d126707676f8c8b0c806ec22dc9f9aab64d5893
zahareliseev <zakh-e@yandex.ru>
2020-10-13 00:54:56 +0300

    Added owner uage

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hamcrest.Matchers;
@@ -13,7 +14,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class HomePageTest {
 
     private static final Logger logger = LogManager.getLogger(HomePageTest.class);
-    protected static WebDriver driver;
+    private static WebDriver driver;
+    private static final DomainConfig cfg = ConfigFactory.create(DomainConfig.class);
+
     private final static String HOMEPAGE_TITLE = "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
 
     @BeforeTest
@@ -25,7 +28,7 @@ public class HomePageTest {
 
     @Test
     public void openHomePage() {
-        driver.get("https://otus.ru");
+        driver.get(cfg.hostname());
         logger.info("Main page was open");
         assertThat(driver.getTitle(), Matchers.equalTo(HOMEPAGE_TITLE));
     }
