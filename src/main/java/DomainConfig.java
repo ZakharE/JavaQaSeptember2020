251995e724a9a70a8e87ae8c18928a09576941d9
zahareliseev <zakh-e@yandex.ru>
2020-11-06 01:04:38 +0300

    Refactor test

--- src/main/java/DomainConfig.java
+++ src/main/java/DomainConfig.java
@@ -3,4 +3,5 @@ import org.aeonbits.owner.Config;
 public interface DomainConfig extends Config {
 
     String hostname();
+    String voltMainPage();
 }
--- src/test/java/HomeWork4.java
+++ src/test/java/HomeWork4.java
@@ -15,6 +15,10 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
 public class HomeWork4 {
     private static final Logger logger = LogManager.getLogger(HomePageTest.class);
     private static WebDriver driver;
@@ -32,46 +36,76 @@ public class HomeWork4 {
 
     @Test
     public void addTwoItemsToCart() {
-        driver.get(cfg.hostname());
+        driver.get(cfg.voltMainPage());
+        selectCity("city7800000000000");
+        selectCategoty("Электроинструменты");
+        selectCategoty("Перфораторы");
+        waitElementToBeClickable(By.cssSelector("div.producer-list-toggle"));
+        String makita = "MAKITA";
+        addProductToFilter(makita);
+        String zubr = "ЗУБР";
+        addProductToFilter(zubr);
+        applyFilter();
+        sortBy(By.cssSelector("ul[class='select2-results__options']> li:nth-child(1)"));
+
+        String zubrId = addProductToCompareList(zubr);
+        String makitaId = addProductToCompareList(makita);
+        goToCompareList();
+        assertCompareListHasProductWithId(makitaId);
+        assertCompareListHasProductWithId(zubrId);
+    }
+
+    private void assertCompareListHasProductWithId(String id) {
+        List<WebElement> products = driver.findElements(By.id(id + "_compare"));
+        assertThat(products, hasSize(1));
+    }
+
+    private void goToCompareList() {
+        driver.findElement(By.id("cCountCompare")).click();
+    }
+
+    private String addProductToCompareList(String product) {
+        WebElement item = driver.findElements(By.xpath(String.format("//ul/li/*[contains(text(), '%s')]/..", product))).get(0);
+        String id = item.findElement(By.cssSelector(".new-item-list-name > a")).getAttribute("data-code");
+        new WebDriverWait(driver, 10)
+                .until(visibilityOf(item.findElement(By.cssSelector("div.new-item-list-compare")))).click();
+        By closePopUpCross = By.cssSelector(".ui-button");
+        driver.findElement(closePopUpCross).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.invisibilityOfElementLocated(closePopUpCross));
+        return id;
+    }
+
+    private void sortBy(By sortSelector) {
+        waitElementToBeClickable(By.cssSelector("span[class='select2-listing'"));
+        driver.findElement(sortSelector).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector("#product-list > li"), 0));
+    }
+
+    private void applyFilter() {
+        driver.findElement(By.cssSelector("input[value='Подобрать модель']")).click();
+    }
+
+    private void addProductToFilter(String product) {
+        driver.findElement(By.cssSelector(String.format("input[title='%s']", product))).click();
+    }
+
+    private void waitElementToBeClickable(By selector) {
+        new WebDriverWait(driver, 10)
+                .until(elementToBeClickable(selector)).click();
+    }
+
+    private void selectCategoty(String category) {
+        new WebDriverWait(driver, 10)
+                .until(visibilityOfElementLocated(By.xpath(String.format(".//li/a[@title=\"%s\"]/..", category))))
+                .click();
+    }
+
+    private void selectCity(String cityLabel) {
         new WebDriverWait(driver, 10)
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("modal-select-country")))
-                .findElement(By.cssSelector("label[for='city7800000000000']"))
+                .findElement(By.cssSelector(String.format("label[for='%s']", cityLabel)))
                 .click();
-
         new WebDriverWait(driver, 10).until(ExpectedConditions.invisibilityOfElementLocated(By.id("modal-select-country")));
-
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.elementToBeClickable(By.xpath(".//li/a[@title=\"Электроинструменты\"]/..")))
-                .click();
-
-
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.elementToBeClickable(By.xpath(".//li/a[@title=\"Перфораторы\"]/..")))
-                .click();
-
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.producer-list-toggle"))).click();
-        driver.findElement(By.cssSelector("input[title='MAKITA']")).click();
-        driver.findElement(By.cssSelector("input[title='ЗУБР']")).click();
-        driver.findElement(By.cssSelector("input[value='Подобрать модель']")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[class='select2-listing'"))).click();
-        driver.findElement(By.cssSelector("ul[class='select2-results__options']> li:nth-child(1)")).click();
-        new WebDriverWait(driver, 10).until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//ul/li/*[contains(text(), 'ЗУБР')]/.."), 0));
-        WebElement item = driver.findElements(By.xpath("//ul/li/*[contains(text(), 'ЗУБР')]/..")).get(0);
-        String zubr = item.findElement(By.cssSelector(".new-item-list-name > a")).getAttribute("data-code");
-
-        item.findElement(By.cssSelector("div.new-item-list-compare")).click();
-        driver.findElement(By.cssSelector(".ui-button")).click();
-        item = driver.findElements(By.xpath("//ul/li/*[contains(text(), 'MAKITA')]/..")).get(0);
-        item.findElement(By.cssSelector("div.new-item-list-compare")).click();
-        String makita = item.findElement(By.tagName(".new-item-list-name > a")).getAttribute("data-code");
-        driver.findElement(By.cssSelector(".ui-button")).click();
-        driver.findElement(By.id("cCountCompare")).click();
-        List<WebElement> elements = driver.findElements(By.id(makita + "_compare"));
-        List<WebElement> elements1 = driver.findElements(By.id(zubr + "_compare"));
-        assert elements.size() == 1;
-        assert elements1.size() == 1;
-
     }
 
     @AfterTest
