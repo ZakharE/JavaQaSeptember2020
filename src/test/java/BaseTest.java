27d31fb83324b7e49f93d427439c85ef5c26325f
zahareliseev <zakh-e@yandex.ru>
2021-02-15 23:59:05 +0300

    Fix remote execution

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -6,10 +7,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTest {
     protected static final Logger logger = LogManager.getLogger(HomePageTest.class);
@@ -23,8 +28,17 @@ public class BaseTest {
     public void createNewSession() {
         WebDriverManager.chromedriver().setup();
 
-        WebDriverRunner.setWebDriver(new ChromeDriver());
-        logger.info("Driver initiated");
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("safebrowsing.enabled", true);
+        prefs.put("profile.default_content_settings.popups", 0);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("disable-extensions");
+        options.addArguments("safebrowsing-disable-extension-blacklist");
+        options.addArguments("safebrowsing-disable-download-protection");
+
+        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);        logger.info("Driver initiated");
         Selenide.open("https://events.epam.com/");
         Selenide.clearBrowserCookies();
     }
